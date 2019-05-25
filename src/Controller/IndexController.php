fcbc36f83eb1e6a933db645e61df0e81409f3173
maksimes <maksimes@mail.ru>
2019-05-25 15:38:30 +0300

    example template and controllers

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 024 24.05.2019
+ * Time: 23:55
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController
+{
+    /**
+     * @Route("/index/{number}")
+     */
+    public function showAction($number)
+    {
+        return new Response(sprintf('Hello! Test project: %s', $number));
+    }
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'Реализация намеченных плановых заданий процветает, как ни в чем не бывало',
+            'Частокол на границе починят', 'Главные СМИ предупреждают: зима близко',
+            'Как бы то ни было, доблесть наших правозащитников разочаровала'
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
+    }
+}
